880a63f187d8ca7f966b933b51c255661a25ec7f
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-02 11:20:59 +0800

    done

--- configs/mode2_se.py
+++ configs/mode2_se.py
@@ -3,14 +3,10 @@ _base_ = ['_base_/default_runtime.py',
           '_base_/models/fusion_4branch.py',
           '_base_/datasets/base_dataset.py']
 
+load_from = 'pretrained/text0.717_audio0.675_video0.707_image0.706.pth'
+
 model = dict(
     mode=2,
-    pretrained=dict(
-        image='pretrained/image_0.7061.pth',
-        text='pretrained/text_0.7176.pth',
-        audio='pretrained/audio_0.6759.pth',
-        video='pretrained/video_0.7072.pth',
-    ),
     head_config=dict(
         fusion=dict(type='SEHead', in_dim=20480, out_dim=82,
                     gating_reduction=8, hidden_size=1024,
---
+++ tools/merge_branch.py
@@ -0,0 +1,31 @@
+import argparse
+
+import torch
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', help='checkpoint file')
+    parser.add_argument('--image', help='checkpoint file')
+    parser.add_argument('--video', help='checkpoint file')
+    parser.add_argument('--audio', help='checkpoint file')
+    parser.add_argument('out', help='checkpoint file')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    text_ckpt = torch.load(args.text)
+    image_ckpt = torch.load(args.image)
+    video_ckpt = torch.load(args.video)
+    audio_ckpt = torch.load(args.audio)
+    ret = {}
+    for typ, dic in zip(['text', 'image', 'video', 'audio'],
+                         [text_ckpt, image_ckpt, video_ckpt, audio_ckpt]):
+        for key, val in dic.items():
+            if key.startswith(typ):
+                ret[key] = val
+    torch.save(ret, args.out)
+    print(f'Saved as {args.out}')
+
+if __name__=='__main__':
+    main()

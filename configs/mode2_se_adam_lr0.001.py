557448ba562f1fbe3328745eb75c489336066214
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-02 12:00:06 +0800

    upd

---
+++ configs/mode2_se_adam_lr0.001.py
@@ -0,0 +1,18 @@
+_base_ = ['_base_/default_runtime.py',
+          '_base_/schedules/schedule_1x.py',
+          '_base_/models/fusion_4branch.py',
+          '_base_/datasets/base_dataset.py']
+
+load_from = 'pretrained/text0.717_audio0.675_video0.707_image0.706.pth'
+
+model = dict(
+    mode=2,
+    head_config=dict(
+        fusion=dict(type='SEHead', in_dim=20480, out_dim=82,
+                    gating_reduction=8, hidden_size=1024,
+                    loss=dict(type='MultiLabelBCEWithLogitsLoss'))
+    )
+)
+data = dict(workers_per_gpu=2)
+
+optimizer = dict(_delete_=True, type='Adam', lr=0.001, weight_decay=0.0001)

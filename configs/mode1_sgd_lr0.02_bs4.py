eeb88d292b5aa7bfeb59e00d210ad416870bafe5
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-02 08:32:17 +0800

    upd

---
+++ configs/mode1_sgd_lr0.02_bs4.py
@@ -0,0 +1,13 @@
+_base_ = ['_base_/default_runtime.py',
+          '_base_/schedules/schedule_1x.py',
+          '_base_/models/fusion_4branch.py',
+          '_base_/datasets/base_dataset.py']
+
+model = dict(
+    mode=1,
+)
+
+data=dict(samples_per_gpu=2,
+          workers_per_gpu=2)
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)

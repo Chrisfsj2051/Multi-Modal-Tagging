856ff90c6ba8840366728c85e87a2a8dfae04ea4
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-03 20:22:46 +0800

    done

---
+++ configs/mode1_image_aug.py
@@ -0,0 +1,46 @@
+_base_ = ['_base_/default_runtime.py',
+          '_base_/schedules/schedule_1x.py',
+          '_base_/models/fusion_4branch.py',
+          '_base_/datasets/base_dataset.py']
+
+
+
+model = dict(
+    mode=1,
+    pretrained=dict(
+        _delete_=True
+    ),
+    modal_used=['image'],
+    branch_config=dict(
+        image_branch=dict(
+            plugins=[
+                dict(
+                    cfg=dict(
+                        type='DropBlock',
+                        drop_prob=0.05,
+                        block_size=7,
+                        postfix='_1'),
+                    stages=(False, False, True, True),
+                    position='after_conv1'),
+                dict(
+                    cfg=dict(
+                        type='DropBlock',
+                        drop_prob=0.05,
+                        block_size=7,
+                        postfix='_2'),
+                    stages=(False, False, True, True),
+                    position='after_conv2'),
+                dict(
+                    cfg=dict(
+                        type='DropBlock',
+                        drop_prob=0.05,
+                        block_size=7,
+                        postfix='_3'),
+                    stages=(False, False, True, True),
+                    position='after_conv3')
+            ]
+        )
+    )
+)
+
+optimizer = dict(_delete_=True, type='SGD', lr=0.02, weight_decay=0.0001)
--- mmt/datasets/pipelines/transforms.py
+++ mmt/datasets/pipelines/transforms.py
@@ -223,9 +223,155 @@ class TextOfflineAug(object):
             return results
         assert 'id_name' in results.keys()
         for key in ['video_asr', 'video_orc']:
-            data_path = self.aug_root + '/' + results['id_name'] + '/' +key + '/'
+            data_path = self.aug_root + '/' + results['id_name'] + '/' + key + '/'
             file = random.choice(os.listdir(data_path))
             with open(data_path + file, 'r', encoding='utf-8') as f:
                 results['text'][key] = f.read().strip()
 
         return results
+
+
+@PIPELINES.register_module()
+class RandomFlip(object):
+    def __init__(self, flip_ratio=None, direction='horizontal'):
+        if isinstance(flip_ratio, list):
+            assert mmcv.is_list_of(flip_ratio, float)
+            assert 0 <= sum(flip_ratio) <= 1
+        elif isinstance(flip_ratio, float):
+            assert 0 <= flip_ratio <= 1
+        elif flip_ratio is None:
+            pass
+        else:
+            raise ValueError('flip_ratios must be None, float, '
+                             'or list of float')
+        self.flip_ratio = flip_ratio
+
+        valid_directions = ['horizontal', 'vertical', 'diagonal']
+        if isinstance(direction, str):
+            assert direction in valid_directions
+        elif isinstance(direction, list):
+            assert mmcv.is_list_of(direction, str)
+            assert set(direction).issubset(set(valid_directions))
+        else:
+            raise ValueError('direction must be either str or list of str')
+        self.direction = direction
+
+        if isinstance(flip_ratio, list):
+            assert len(self.flip_ratio) == len(self.direction)
+
+    def __call__(self, results):
+        use_flip, flip_direction = None, None
+        if 'flip' not in results:
+            if isinstance(self.direction, list):
+                # None means non-flip
+                direction_list = self.direction + [None]
+            else:
+                # None means non-flip
+                direction_list = [self.direction, None]
+
+            if isinstance(self.flip_ratio, list):
+                non_flip_ratio = 1 - sum(self.flip_ratio)
+                flip_ratio_list = self.flip_ratio + [non_flip_ratio]
+            else:
+                non_flip_ratio = 1 - self.flip_ratio
+                # exclude non-flip
+                single_ratio = self.flip_ratio / (len(direction_list) - 1)
+                flip_ratio_list = [single_ratio] * (len(direction_list) -
+                                                    1) + [non_flip_ratio]
+
+            cur_dir = np.random.choice(direction_list, p=flip_ratio_list)
+
+            use_flip = cur_dir is not None
+
+        flip_direction = cur_dir
+        if use_flip:
+            results['image'] = mmcv.imflip(
+                results['image'], direction=flip_direction)
+
+        return results
+
+    def __repr__(self):
+        return self.__class__.__name__ + f'(flip_ratio={self.flip_ratio})'
+
+
+
+
+
+@PIPELINES.register_module()
+class PhotoMetricDistortion(object):
+
+    def __init__(self,
+                 brightness_delta=32,
+                 contrast_range=(0.5, 1.5),
+                 saturation_range=(0.5, 1.5),
+                 hue_delta=18):
+        self.brightness_delta = brightness_delta
+        self.contrast_lower, self.contrast_upper = contrast_range
+        self.saturation_lower, self.saturation_upper = saturation_range
+        self.hue_delta = hue_delta
+
+    def __call__(self, results):
+
+        if 'img_fields' in results:
+            assert results['img_fields'] == ['img'], \
+                'Only single img_fields is allowed'
+        ori_dtype = results['image'].dtype
+        img = results['image'].astype(np.float32)
+        assert img.dtype == np.float32, \
+            'PhotoMetricDistortion needs the input image of dtype np.float32,'\
+            ' please set "to_float32=True" in "LoadImageFromFile" pipeline'
+        # random brightness
+        if random.randint(2):
+            delta = random.uniform(-self.brightness_delta,
+                                   self.brightness_delta)
+            img += delta
+
+        # mode == 0 --> do random contrast first
+        # mode == 1 --> do random contrast last
+        mode = random.randint(2)
+        if mode == 1:
+            if random.randint(2):
+                alpha = random.uniform(self.contrast_lower,
+                                       self.contrast_upper)
+                img *= alpha
+
+        # convert color from BGR to HSV
+        img = mmcv.bgr2hsv(img)
+
+        # random saturation
+        if random.randint(2):
+            img[..., 1] *= random.uniform(self.saturation_lower,
+                                          self.saturation_upper)
+
+        # random hue
+        if random.randint(2):
+            img[..., 0] += random.uniform(-self.hue_delta, self.hue_delta)
+            img[..., 0][img[..., 0] > 360] -= 360
+            img[..., 0][img[..., 0] < 0] += 360
+
+        # convert color from HSV to BGR
+        img = mmcv.hsv2bgr(img)
+
+        # random contrast
+        if mode == 0:
+            if random.randint(2):
+                alpha = random.uniform(self.contrast_lower,
+                                       self.contrast_upper)
+                img *= alpha
+
+        # randomly swap channels
+        if random.randint(2):
+            img = img[..., random.permutation(3)]
+
+        results['image'] = img.to(ori_dtype)
+        return results
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(\nbrightness_delta={self.brightness_delta},\n'
+        repr_str += 'contrast_range='
+        repr_str += f'{(self.contrast_lower, self.contrast_upper)},\n'
+        repr_str += 'saturation_range='
+        repr_str += f'{(self.saturation_lower, self.saturation_upper)},\n'
+        repr_str += f'hue_delta={self.hue_delta})'
+        return repr_str

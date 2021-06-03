4c05d78f177b6cd8551eb4e174360e23dd84fb8f
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-03 19:33:40 +0800

    upd

--- mmt/datasets/pipelines/transforms.py
+++ mmt/datasets/pipelines/transforms.py
@@ -216,20 +216,22 @@ class FrameRandomSwap(FrameAugBox):
 class TextAugBox(object):
 
     def __init__(self, random_rate=0.0, similar_rate=0.0, homophone_rate=0.0, delete_rate=0.0, exchange_rate=0.0,  equivalen_rate=0.0):
-        self.aug_list = []
+        # self.aug_list = []
+
         create_num = 3
-        if random_rate > 0:
-            self.aug_list.append(nlpcda.Randomword(create_num=create_num, change_rate=random_rate))
-        if similar_rate > 0:
-            self.aug_list.append(nlpcda.Similarword(create_num=create_num, change_rate=similar_rate))
-        if homophone_rate >0 :
-            self.aug_list.append(nlpcda.Homophone(create_num=create_num, change_rate=homophone_rate))
-        if delete_rate > 0:
-            self.aug_list.append(nlpcda.RandomDeleteChar(create_num=create_num, change_rate=delete_rate))
-        if exchange_rate > 0:
-            self.aug_list.append(nlpcda.CharPositionExchange(create_num=create_num, change_rate=exchange_rate,char_gram=3))
-        if equivalen_rate > 0:
-            self.aug_list.append(nlpcda.EquivalentChar(create_num=create_num, change_rate=equivalen_rate))
+        self.aug = nlpcda.Randomword(create_num=create_num, change_rate=random_rate)
+        # if random_rate > 0:
+        #     self.aug_list.append(nlpcda.Randomword(create_num=create_num, change_rate=random_rate))
+        # if similar_rate > 0:
+        #     self.aug_list.append(nlpcda.Similarword(create_num=create_num, change_rate=similar_rate))
+        # if homophone_rate >0 :
+        #     self.aug_list.append(nlpcda.Homophone(create_num=create_num, change_rate=homophone_rate))
+        # if delete_rate > 0:
+        #     self.aug_list.append(nlpcda.RandomDeleteChar(create_num=create_num, change_rate=delete_rate))
+        # if exchange_rate > 0:
+        #     self.aug_list.append(nlpcda.CharPositionExchange(create_num=create_num, change_rate=exchange_rate,char_gram=3))
+        # if equivalen_rate > 0:
+        #     self.aug_list.append(nlpcda.EquivalentChar(create_num=create_num, change_rate=equivalen_rate))
 
 
     def apply_aug(self, text):
@@ -238,6 +240,7 @@ class TextAugBox(object):
         return text
 
     def __call__(self, results):
+        return results
         random.shuffle(self.aug_list)
         results['text']['video_ocr'] = self.apply_aug(results['text']['video_ocr'])
         results['text']['video_asr'] = self.apply_aug(results['text']['video_asr'])
---
+++ tools/generate_text_aug.py
@@ -0,0 +1,57 @@
+import json
+import os
+import random
+from multiprocessing import Pool
+
+import nlpcda
+import mmcv
+from tqdm import tqdm
+
+# nlpcda.Homophone(create_num=3, change_rate=0.3),
+
+
+all_aug_list = [
+    nlpcda.Randomword(create_num=3, change_rate=0.1),
+    nlpcda.Similarword(create_num=3, change_rate=0.08),
+    nlpcda.RandomDeleteChar(create_num=3, change_rate=0.05),
+    nlpcda.CharPositionExchange(create_num=3, change_rate=0.05, char_gram=3),
+    nlpcda.EquivalentChar(create_num=3, change_rate=0.08)
+]
+
+single_aug_list = [
+    nlpcda.Randomword(create_num=3, change_rate=0.2),
+    nlpcda.Similarword(create_num=3, change_rate=0.2),
+    nlpcda.RandomDeleteChar(create_num=3, change_rate=0.2),
+    nlpcda.CharPositionExchange(create_num=3, change_rate=0.2, char_gram=3),
+    nlpcda.EquivalentChar(create_num=3, change_rate=0.2)
+]
+
+save_path = 'dataset/text_aug/'
+data_root = 'dataset/tagging/tagging_dataset_train_5k/text_txt/tagging/'
+mmcv.mkdir_or_exist(save_path)
+
+def go(data):
+    print(data)
+    id_name = data.split('.')[0]
+    cur_save_path = save_path + id_name + '/'
+    with open(data_root + data, 'r', encoding='utf-8') as f:
+        results = json.load(f)
+    for key in ['video_ocr', 'video_asr']:
+        mmcv.mkdir_or_exist(cur_save_path + key)
+        text = results[key].replace('|', '，')
+        aug_results = [text]
+        for aug in single_aug_list:
+            for _ in range(3):
+                aug_results.append(aug.replace(text))
+        random.shuffle(all_aug_list)
+
+        for cnt in range(6):
+            text = aug_results[0]
+            for aug in all_aug_list:
+                text = aug.replace(text)[-1]
+            aug_results.append(text)
+        with open(cur_save_path + key + f'/{cnt}.txt', 'w', encoding='utf-8') as f:
+            f.write(text)
+
+pool = Pool(8)
+pool.map(go, os.listdir(data_root))

9453c6cd1fa1fb073df97b807a50dc1b4105b142
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-05 22:41:22 +0800

    upd config

---
+++ configs/mode1_image_dropblock.py
@@ -0,0 +1,50 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x.py',
+    '_base_/models/fusion_4branch.py', '_base_/datasets/base_dataset.py'
+]
+
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375])
+
+train_pipeline = [
+    dict(type='LoadAnnotations'),
+    # dict(type='PhotoMetricDistortion',
+    #      brightness_delta=16,
+    #      contrast_range=(0.75, 1.25),
+    #      saturation_range=(0.75, 1.25),
+    #      hue_delta=9),
+    # dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Tokenize', vocab_root='dataset/vocab_small.txt',
+         max_length=256),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='FrameRandomErase',
+         key_fields=['video'],
+         aug_num_frame=30,
+         aug_max_len=10,
+         aug_num_block=10,
+         aug_max_size=100),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'image', 'text', 'audio', 'gt_labels'])
+]
+
+model = dict(
+    mode=1,
+    modal_used=['image'],
+    branch_config=dict(image=dict(plugins=[
+        dict(cfg=dict(
+            type='DropBlock', drop_prob=0.05, block_size=7, postfix='_1'),
+             stages=(False, False, True, True),
+             position='after_conv1'),
+        dict(cfg=dict(
+            type='DropBlock', drop_prob=0.05, block_size=7, postfix='_2'),
+             stages=(False, False, True, True),
+             position='after_conv2'),
+        dict(cfg=dict(
+            type='DropBlock', drop_prob=0.05, block_size=7, postfix='_3'),
+             stages=(False, False, True, True),
+             position='after_conv3')
+    ])),
+    ebd_config=dict(image=dict(type='FCHead')))
+data = dict(train=dict(pipeline=train_pipeline))

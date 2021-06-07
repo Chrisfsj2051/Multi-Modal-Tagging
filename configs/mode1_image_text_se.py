8fd38b8eaeb2553371716fc10b626329608357a5
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-07 11:05:47 +0800

    upd

---
+++ configs/mode1_image_text_se.py
@@ -0,0 +1,39 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x.py',
+    '_base_/models/fusion_4branch.py', '_base_/datasets/base_dataset.py'
+]
+
+model = dict(mode=1,
+             modal_used=['text', 'image'],
+             ebd_config=dict(
+                 video=dict(
+                     type='SEHead',
+                     in_dim=16384,
+                     gating_reduction=8,
+                     out_dim=1024
+                 ),
+                 image=dict(
+                     type='SEHead',
+                     in_dim=2048,
+                     gating_reduction=8,
+                     out_dim=1024
+                 ),
+                 text=dict(
+                     type='SEHead',
+                     in_dim=1024,
+                     gating_reduction=8,
+                     out_dim=1024
+                 ),
+                 audio=dict(
+                     type='SEHead',
+                     in_dim=1024,
+                     gating_reduction=8,
+                     out_dim=1024
+                 ),
+             ),
+             )
+optimizer = dict(_delete_=True,
+                 type='SGD',
+                 momentum=0.9,
+                 lr=0.02,
+                 weight_decay=0.0001)
--- configs/mode2_fc.py
+++ configs/mode2_fc.py
@@ -5,9 +5,24 @@ _base_ = [
 
 load_from = 'pretrained/text0.7178_audio0.6702_video0.7139_image0.7039.pth'
 
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375])
+
+train_pipeline = [
+    dict(type='LoadAnnotations'),
+    dict(type='Tokenize', vocab_root='dataset/vocab_small.txt',
+         max_length=256),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'image', 'text', 'audio', 'gt_labels'])
+]
+
+data = dict(train=dict(pipeline=train_pipeline))
+
+
 model = dict(mode=2,
-             modal_dropout_p=dict(text=0.3, video=0.3, image=0.3, audio=0.3),
-             attn_config=dict(input_dropout_p=0.3),
              head_config=dict(
                  fusion=dict(
                      _delete_=True,

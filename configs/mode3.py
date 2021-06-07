4cd25f552b5587770fc151bcc04d9062f703f88b
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-07 13:59:36 +0800

    upd

--- configs/mode3.py
+++ configs/mode3.py
@@ -3,7 +3,7 @@ _base_ = [
     '_base_/models/fusion_4branch.py', '_base_/datasets/base_dataset.py'
 ]
 
-load_from = 'work_dirs/mode2/iter_10000.pth'
+# load_from = 'work_dirs/mode2/iter_10000.pth'
 
 model = dict(mode=3)
-optimizer = dict(_delete_=True, type='Adam', lr=0.0001, weight_decay=0.0001)
+optimizer = dict(_delete_=True, type='Adam', lr=0.001, weight_decay=0.0001)

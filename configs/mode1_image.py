61c15842360604a7aed3057c41a4725b46192df8
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-10 14:02:26 +0800

    Create mode1_image.py

---
+++ configs/mode1_image.py
@@ -0,0 +1,18 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x.py',
+    '_base_/models/fusion_4branch.py', '_base_/datasets/base_dataset.py'
+]
+
+model = dict(
+    mode=1, modal_used=['image'],
+    pretrained=dict(
+        # image='pretrained/resnet50_batch256_imagenet_20200708-cfb998bf.pth'
+        image='torchvision://resnet101'
+    ),
+    branch_config=dict(image=dict(depth=101))
+)
+optimizer = dict(_delete_=True,
+                 type='SGD',
+                 momentum=0.9,
+                 lr=0.02,
+                 weight_decay=0.0001)

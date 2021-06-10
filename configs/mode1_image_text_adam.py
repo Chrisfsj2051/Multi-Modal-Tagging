3303b2a1746548611634ca0a0d56314934638034
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-10 13:29:22 +0800

    Create mode1_image_text_adam.py

---
+++ configs/mode1_image_text_adam.py
@@ -0,0 +1,16 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x.py',
+    '_base_/models/fusion_4branch.py', '_base_/datasets/base_dataset.py'
+]
+
+model = dict(mode=1, modal_used=['text', 'image'])
+
+optimizer = dict(_delete_=True, type='Adam', lr=0.001, weight_decay=0.0001)
+
+data = dict(samples_per_gpu=4)
+
+# optimizer = dict(_delete_=True,
+#                  type='SGD',
+#                  momentum=0.9,
+#                  lr=0.02,
+#                  weight_decay=0.0001)

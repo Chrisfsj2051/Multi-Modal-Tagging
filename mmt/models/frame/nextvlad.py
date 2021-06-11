59b9e8b8d6db52e6beaa2b6530df8cd206c9a2cd
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-11 22:09:12 +0800

    upd cfg

--- mmt/models/frame/nextvlad.py
+++ mmt/models/frame/nextvlad.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from mmcv.cnn import build_norm_layer
 from torch.nn import init
 
 from mmt.models.builder import FRAME
@@ -10,10 +11,12 @@ from mmt.models.builder import FRAME
 @FRAME.register_module()
 class NeXtVLAD(nn.Module):
     """NetVLAD layer implementation."""
+
     def __init__(self,
                  feature_size,
                  max_frames,
                  cluster_size,
+                 norm_cfg=dict(type='BN1d'),
                  expansion=2,
                  groups=16):
         super(NeXtVLAD, self).__init__()
@@ -30,9 +33,11 @@ class NeXtVLAD(nn.Module):
         self.w4 = nn.Parameter(
             torch.randn(1, self.expansion * self.feature_size // self.groups,
                         cluster_size))
-        self.bn1 = nn.BatchNorm1d(groups * cluster_size)
-        self.bn2 = nn.BatchNorm1d(self.cluster_size * self.expansion *
-                                  self.feature_size // self.groups)
+
+        self.bn1 = build_norm_layer(norm_cfg, groups * cluster_size)
+        self.bn2 = build_norm_layer(norm_cfg,
+                                    self.cluster_size * self.expansion *
+                                    self.feature_size // self.groups)
         self.init_parameters()
 
     def init_parameters(self):

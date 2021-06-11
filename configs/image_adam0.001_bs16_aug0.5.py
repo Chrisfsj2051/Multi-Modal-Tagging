7a1615616534790695449c8180730c482df75256
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-11 19:44:06 +0800

    upd cfg

---
+++ configs/image_adam0.001_bs16_aug0.5.py
@@ -0,0 +1,29 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x_adam.py',
+    '_base_/models/single_branch.py', '_base_/datasets/base_dataset.py'
+]
+
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375])
+
+train_pipeline = [
+    dict(type='LoadAnnotations'),
+    dict(type='PhotoMetricDistortion',
+         brightness_delta=16,
+         contrast_range=(0.75, 1.25),
+         saturation_range=(0.75, 1.25),
+         hue_delta=9),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Tokenize', vocab_root='dataset/vocab_small.txt',
+         max_length=256),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'image', 'text', 'audio', 'gt_labels'])
+]
+
+model = dict(modal_used=['image'])
+data = dict(samples_per_gpu=8,
+            workers_per_gpu=8,
+            train=dict(pipeline=train_pipeline))
---
+++ configs/image_adamW_bs16.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x_adam.py',
+    '_base_/models/single_branch.py', '_base_/datasets/base_dataset.py'
+]
+
+model=dict(modal_used=['image'])
+optimizer = dict(type='AdamW')
+data = dict(samples_per_gpu=8, workers_per_gpu=8)

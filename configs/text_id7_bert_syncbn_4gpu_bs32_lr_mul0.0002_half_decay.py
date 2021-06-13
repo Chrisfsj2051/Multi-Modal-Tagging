8ccfd8460a8752b6a91963be4c11b911ac0f3429
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-13 18:54:13 +0800

    Create text_id7_bert_syncbn_4gpu_bs32_lr_mul0.0002_half_decay.py

---
+++ configs/text_id7_bert_syncbn_4gpu_bs32_lr_mul0.0002_half_decay.py
@@ -0,0 +1,19 @@
+_base_ = 'text_id7_bert_syncbn_4gpu.py'
+
+data = dict(samples_per_gpu=8, workers_per_gpu=8)
+
+optimizer = dict(lr=0.1,
+                 paramwise_cfg=dict(
+                     custom_keys={
+                         'image_branch': dict(lr_mult=0.01, decay_mult=1.0),
+                         'text_branch': dict(lr_mult=0.0001, decay_mult=1.0),
+                         'video_branch': dict(lr_mult=0.01, decay_mult=1.0),
+                         'audio_branch': dict(lr_mult=0.01, decay_mult=1.0),
+                         'fusion': dict(weight_decay_mult=1.0)
+                     }))
+train_total_iters = 10000
+lr_config = dict(policy='step',
+                 warmup='linear',
+                 warmup_iters=500,
+                 warmup_ratio=0.001,
+                 step=[train_total_iters // 2, 3 * train_total_iters // 4])

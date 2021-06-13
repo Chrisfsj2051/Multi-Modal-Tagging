cd6b690fecd938d7a62f84b1a54944269154ff3f
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-14 01:25:45 +0800

    Update text_id7_bert_bs32_hybrid_drop0.2_1.5x.py

--- configs/text_id7_bert_bs32_hybrid_drop0.2_1.5x.py
+++ configs/text_id7_bert_bs32_hybrid_drop0.2_1.5x.py
@@ -3,19 +3,19 @@ _base_ = 'text_id6_bert.py'
 norm_cfg = dict(type='SyncBN')
 # norm_cfg = dict(type='BN1d')
 
-data = dict(samples_per_gpu=32, workers_per_gpu=8)
+data = dict(samples_per_gpu=16, workers_per_gpu=8)
 
 model = dict(modal_used=['text'],
              branch_config=dict(text=dict(_delete_=True, type='Bert')),
              head_config=dict(image=dict(dropout_p=0.8, norm_cfg=norm_cfg),
                               video=dict(norm_cfg=norm_cfg),
-                              text=dict(dropout_p=0.5,
+                              text=dict(dropout_p=0.2,
                                         norm_cfg=norm_cfg,
                                         in_dim=768),
                               audio=dict(norm_cfg=norm_cfg),
                               fusion=dict(norm_cfg=norm_cfg)))
 
-train_total_iters = 20000
+train_total_iters = 15000
 lr_config = dict(policy='step',
                  warmup='linear',
                  warmup_iters=500,

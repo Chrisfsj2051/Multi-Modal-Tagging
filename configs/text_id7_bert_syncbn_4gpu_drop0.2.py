94a3746d3d1a4c537340450182f97d11600e7919
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-13 21:51:17 +0800

    Create text_id7_bert_syncbn_4gpu_drop0.2.py

---
+++ configs/text_id7_bert_syncbn_4gpu_drop0.2.py
@@ -0,0 +1,16 @@
+_base_ = 'text_id6_bert.py'
+
+norm_cfg = dict(type='SyncBN')
+# norm_cfg = dict(type='BN1d')
+
+data = dict(samples_per_gpu=4, workers_per_gpu=8)
+
+model = dict(modal_used=['text'],
+             branch_config=dict(text=dict(_delete_=True, type='Bert')),
+             head_config=dict(image=dict(dropout_p=0.8, norm_cfg=norm_cfg),
+                              video=dict(norm_cfg=norm_cfg),
+                              text=dict(dropout_p=0.2,
+                                        norm_cfg=norm_cfg,
+                                        in_dim=768),
+                              audio=dict(norm_cfg=norm_cfg),
+                              fusion=dict(norm_cfg=norm_cfg)))

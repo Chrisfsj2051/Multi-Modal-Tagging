61dac895e98a9a48c318cd206226d40a44380798
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-22 23:07:41 +0800

    Update video_id3_2gpu_newfeats.py

--- configs/video_id3_2gpu_newfeats.py
+++ configs/video_id3_2gpu_newfeats.py
@@ -7,7 +7,7 @@ train_pipeline = [
     dict(type='LoadAnnotations',
          replace_dict=dict(video=(
              'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
-             'extracted_video_feats/L16_LN'))),
+             'extracted_video_feats/L16_LN/train_5k'))),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
     dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
@@ -18,7 +18,10 @@ train_pipeline = [
 ]
 
 val_pipeline = [
-    dict(type='LoadAnnotations'),
+    dict(type='LoadAnnotations',
+         replace_dict=dict(video=(
+             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/train_5k'))),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
     dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
     dict(type='Resize', size=(224, 224)),

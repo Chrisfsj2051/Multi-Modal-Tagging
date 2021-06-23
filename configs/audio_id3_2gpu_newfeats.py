84a0f8f63d8c3c1951f80be99ec7a9c236d5d1bc
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-06-23 23:52:01 +0800

    upd

--- configs/audio_id3_2gpu_newfeats.py
+++ configs/audio_id3_2gpu_newfeats.py
@@ -1,5 +1,19 @@
 _base_ = 'audio_id3_4gpu.py'
 
+model = dict(
+    type='SingleBranchModel',
+    key='audio',
+    backbone=dict(type='NeXtVLAD',
+                  feature_size=1024,
+                  max_frames=30,
+                  cluster_size=32),
+    #     head=dict(
+    #         type='SingleSEHead',
+    #         in_dim=1024*4,
+    #         dropout_p=0.8
+    #     )
+)
+
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
                     std=[58.395, 57.12, 57.375])
 
@@ -11,11 +25,11 @@ train_pipeline = [
                    'extracted_audio_feats/train_5k'))),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
-    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(30, 1024)),
     dict(type='Resize', size=(224, 224)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['video', 'meta_info', 'gt_labels'])
+    dict(type='Collect', keys=['audio', 'meta_info', 'gt_labels'])
 ]
 
 val_pipeline = [
@@ -25,11 +39,11 @@ val_pipeline = [
             audio=('tagging/tagging_dataset_train_5k/audio_npy/Vggish/tagging',
                    'extracted_audio_feats/train_5k'))),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
-    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(30, 1024)),
     dict(type='Resize', size=(224, 224)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['video', 'meta_info'])
+    dict(type='Collect', keys=['audio', 'meta_info'])
 ]
 
 test_pipeline = [
@@ -39,11 +53,11 @@ test_pipeline = [
             audio=('tagging/tagging_dataset_train_5k/audio_npy/Vggish/tagging',
                    'extracted_audio_feats/test_5k_2nd'))),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
-    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(30, 1024)),
     dict(type='Resize', size=(224, 224)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['video', 'meta_info'])
+    dict(type='Collect', keys=['audio', 'meta_info'])
 ]
 
 data = dict(samples_per_gpu=8,
--- mmt/datasets/pipelines/loading.py
+++ mmt/datasets/pipelines/loading.py
@@ -12,36 +12,49 @@ class LoadAnnotations(object):
     def __init__(self, replace_dict=dict()):
         self.replace_dict = replace_dict
 
-    def replace_anns(self, results):
+    def load_video_anns(self, results):
         if 'video' in self.replace_dict.keys():
             assert self.replace_dict['video'][0] in results['video_anns']
             results['video_anns'] = results['video_anns'].replace(
                 self.replace_dict['video'][0], self.replace_dict['video'][1])
-        return results
+        results['video'] = np.load(results.pop('video_anns')).astype(
+            np.float32)
+
+    def load_audio_anns(self, results):
+        if 'audio' in self.replace_dict.keys():
+            assert self.replace_dict['audio'][0] in results['audio_anns']
+            results['audio_anns'] = results['audio_anns'].replace(
+                self.replace_dict['audio'][0], self.replace_dict['audio'][1])
+        results['audio'] = np.load(results.pop('audio_anns')).astype(
+            np.float32)
+
+    def load_text_anns(self, results):
+        with open(results['text_anns'], 'r', encoding='utf-8') as f:
+            results['text'] = json.load(f)
+        results['text']['video_ocr'] = results['text']['video_ocr'].replace(
+            '|', ',')
+        assert (len(results['text']['video_ocr'])
+                or len(results['text']['video_asr']))
+        if len(results['text']['video_asr']) == 0:
+            results['text']['video_asr'] = deepcopy(
+                results['text']['video_ocr'])
+        if len(results['text']['video_ocr']) == 0:
+            results['text']['video_ocr'] = deepcopy(
+                results['text']['video_asr'])
+
+    def load_image_anns(self, results):
+        results['image'] = mmcv.imread(results['image_anns'])
+        assert results['image'] is not None
 
     def __call__(self, results):
         results = self.replace_anns(results)
         try:
             results['id_name'] = results['audio_anns'].split('/')[-1].split(
                 '.')[0]
-            results['audio'] = np.load(results['audio_anns']).astype(
-                np.float32)
-            results['video'] = np.load(results['video_anns']).astype(
-                np.float32)
-            with open(results['text_anns'], 'r', encoding='utf-8') as f:
-                results['text'] = json.load(f)
-            results['text']['video_ocr'] = results['text'][
-                'video_ocr'].replace('|', ',')
-            assert (len(results['text']['video_ocr'])
-                    or len(results['text']['video_asr']))
-            if len(results['text']['video_asr']) == 0:
-                results['text']['video_asr'] = deepcopy(
-                    results['text']['video_ocr'])
-            if len(results['text']['video_ocr']) == 0:
-                results['text']['video_ocr'] = deepcopy(
-                    results['text']['video_asr'])
-            results['image'] = mmcv.imread(results['image_anns'])
-            assert results['image'] is not None
+            self.load_video_anns(results)
+            self.load_audio_anns(results)
+            self.load_text_anns(results)
+            self.load_image_anns(results)
             return results
         except Exception as e:
             print(e, f' while loading {results["image_anns"]}')

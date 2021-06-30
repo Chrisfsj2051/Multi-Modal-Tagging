3a0e3d42b663611653595dde055e36a3adb353cf
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-06-30 15:33:02 +0800

    upd

--- configs/video_id4_2gpu_semi0.9.py
+++ configs/video_id4_2gpu_semi0.9.py
@@ -4,7 +4,8 @@ img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
                     std=[58.395, 57.12, 57.375])
 
 model = dict(
-    type='SemiSingleBranchModel'
+    type='SemiSingleBranchModel',
+    gt_thr=0.9
 )
 
 train_pipeline = [
@@ -22,14 +23,14 @@ train_pipeline = [
 ]
 
 extra_train_pipeline_1 = [
-    # dict(type='LoadAnnotations',
-    #      replace_dict=dict(video=(
-    #          'tagging/tagging_dataset_test_5k/video_npy/Youtube8M/tagging',
-    #          'extracted_video_feats/L16_LN/test_5k'))),
     dict(type='LoadAnnotations',
          replace_dict=dict(video=(
-             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
-             'extracted_video_feats/L16_LN/train_5k'))),
+             'tagging/tagging_dataset_test_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/test_5k'))),
+    # dict(type='LoadAnnotations',
+    #      replace_dict=dict(video=(
+    #          'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+    #          'extracted_video_feats/L16_LN/train_5k'))),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
     dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
@@ -40,14 +41,14 @@ extra_train_pipeline_1 = [
 ]
 
 extra_train_pipeline_2 = [
-    # dict(type='LoadAnnotations',
-    #      replace_dict=dict(video=(
-    #          'tagging/tagging_dataset_test_5k_2nd/video_npy/Youtube8M/tagging',
-    #          'extracted_video_feats/L16_LN/test_5k_2nd'))),
     dict(type='LoadAnnotations',
          replace_dict=dict(video=(
-             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
-             'extracted_video_feats/L16_LN/train_5k'))),
+             'tagging/tagging_dataset_test_5k_2nd/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/test_5k_2nd'))),
+    # dict(type='LoadAnnotations',
+    #      replace_dict=dict(video=(
+    #          'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+    #          'extracted_video_feats/L16_LN/train_5k'))),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
     dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
@@ -74,16 +75,16 @@ data = dict(
             datasets=[
                 dict(
                     type='TaggingDataset',
-                    # ann_file='dataset/tagging/GroundTruth/datafile/test.txt',  # change to test
-                    ann_file='dataset/tagging/GroundTruth/datafile/train.txt',
+                    ann_file='dataset/tagging/GroundTruth/datafile/test.txt',  # change to test
+                    # ann_file='dataset/tagging/GroundTruth/datafile/train.txt',
                     label_id_file='dataset/tagging/label_super_id.txt',
                     pipeline=extra_train_pipeline_1,
                     test_mode=True
                 ),
                 dict(
                     type='TaggingDataset',
-                    # ann_file='dataset/tagging/GroundTruth/datafile/test_2nd.txt',  # change to test
-                    ann_file='dataset/tagging/GroundTruth/datafile/train.txt',
+                    ann_file='dataset/tagging/GroundTruth/datafile/test_2nd.txt',  # change to test
+                    # ann_file='dataset/tagging/GroundTruth/datafile/train.txt',
                     label_id_file='dataset/tagging/label_super_id.txt',
                     pipeline=extra_train_pipeline_2,
                     test_mode=True
--- mmt/models/fusion/single_branch.py
+++ mmt/models/fusion/single_branch.py
@@ -1,3 +1,5 @@
+import torch
+
 from mmt.models.builder import ARCH, build_backbone, build_head
 from mmt.models.fusion import BaseFusionModel
 
@@ -34,30 +36,44 @@ class SingleBranchModel(BaseFusionModel):
         else:
             return feats, [{self.key: preds}]
 
+
 @ARCH.register_module()
 class SemiSingleBranchModel(SingleBranchModel):
 
-    def __init__(self, **kwargs):
+    def __init__(self, gt_thr, **kwargs):
         super(SemiSingleBranchModel, self).__init__(**kwargs)
         # self.burn_in = True
         self.burn_in = False
+        self.gt_thr = gt_thr
 
-    def unlabeled_forward_train(self, extra_data):
-        print('in')
+    def unlabeled_forward_train(self, **kwargs):
+        # x, meta_info = extra_data.values()
+        self.eval()
+        # EMA
+        with torch.no_grad():
+            pseudo_labels = self.simple_test(return_feats=False, **kwargs)[0].values()
+        assert len(pseudo_labels) == 1
+        pseudo_mask = list(pseudo_labels)[0].sigmoid()
+        pseudo_labels = []
+        for idx in range(pseudo_mask.shape[0]):
+            pseudo_labels.append((pseudo_mask[idx] >= self.gt_thr).nonzero().squeeze())
+        self.train()
+        kwargs['gt_labels'] = pseudo_labels
+        return super(SemiSingleBranchModel, self).forward_train(return_feats=False, **kwargs)
 
     def forward_train(self, return_feats=False, **kwargs):
         extra_data = kwargs.pop('extra')
         losses = super(SemiSingleBranchModel,
-                             self).forward_train(return_feats=return_feats, **kwargs)
+                       self).forward_train(return_feats=return_feats, **kwargs)
         assert not return_feats
         # if return_feats:
         #     feats, losses = losses
         if not self.burn_in:
-            unlabeled_loss = self.unlabeled_forward_train(extra_data)
+            unlabeled_loss = self.unlabeled_forward_train(**extra_data)
             # if return_feats:
             #     feats, losses = losses
             for key in unlabeled_loss.keys():
-                losses[f'ssl_{key}'] = unlabeled_loss.pop(key)
+                losses[f'ssl_{key}'] = unlabeled_loss[key]
         if not return_feats:
             return losses
         else:

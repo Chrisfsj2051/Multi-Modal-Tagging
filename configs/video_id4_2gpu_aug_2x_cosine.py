9802125703c06d32d9c0f839d80c0cf5212f81e9
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-02 16:18:47 +0800

    Update video_id4_2gpu_aug_2x_cosine.py

--- configs/video_id4_2gpu_aug_2x_cosine.py
+++ configs/video_id4_2gpu_aug_2x_cosine.py
@@ -7,11 +7,6 @@ norm_cfg = dict(type='BN1d')
 
 model = dict(head=dict(norm_cfg=norm_cfg))
 
-optimizer = dict(
-    lr=0.01,
-    paramwise_cfg=dict(custom_keys={
-        'backbone': dict(lr_mult=0.01, decay_mult=1.0),
-    }))
 
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
                     std=[58.395, 57.12, 57.375])
@@ -22,12 +17,12 @@ train_pipeline = [
              'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
              'extracted_video_feats/L16_LN/train_5k'))),
     dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
-    dict(type='FrameRandomSwap',
+    dict(type='FrameRandomErase',
          key_fields=['video'],
          aug_num_frame=0,
          aug_max_len=0,
-         aug_num_block=20,
-         aug_max_size=30),
+         aug_num_block=10,
+         aug_max_size=60),
     dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
     dict(type='Resize', size=(224, 224)),
     dict(type='Normalize', **img_norm_cfg),
@@ -41,4 +36,22 @@ data = dict(
     workers_per_gpu=8,
     train=dict(pipeline=train_pipeline)
 )
+
+train_total_iters = 20000
+
+runner = dict(type='IterBasedRunner', max_iters=train_total_iters)
+
+optimizer = dict(
+    lr=0.01,
+    paramwise_cfg=dict(custom_keys={
+        'backbone': dict(lr_mult=0.01, decay_mult=1.0),
+    }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=1.0 / 10,
+    min_lr_ratio=1e-5)
 

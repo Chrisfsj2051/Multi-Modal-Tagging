303f274276c808aad0f41b99d0191331ade170e0
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-02 12:01:07 +0800

    Create video_id4_2gpu_aug.py

---
+++ configs/video_id4_2gpu_aug.py
@@ -0,0 +1,43 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x_adam.py',
+    '_base_/models/video.py', '_base_/datasets/video.py'
+]
+
+norm_cfg = dict(type='BN1d')
+
+model = dict(head=dict(norm_cfg=norm_cfg))
+
+optimizer = dict(
+    lr=0.01,
+    paramwise_cfg=dict(custom_keys={
+        'backbone': dict(lr_mult=0.01, decay_mult=1.0),
+    }))
+
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375])
+
+train_pipeline = [
+    dict(type='LoadAnnotations',
+         replace_dict=dict(video=(
+             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/train_5k'))),
+    dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
+    dict(type='FrameRandomErase',
+         key_fields=['video'],
+         aug_num_frame=5,
+         aug_max_len=1,
+         aug_num_block=0,
+         aug_max_size=0),
+    dict(type='Pad', video_pad_size=(300, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect',
+         keys=['video', 'meta_info', 'gt_labels'])
+]
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(pipeline=train_pipeline)
+)

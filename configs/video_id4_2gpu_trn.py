02de1e8b691e0a8b2dbc83633c3610cd2a1e0067
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-02 21:17:22 +0800

    Update video_id4_2gpu_trn.py

--- configs/video_id4_2gpu_trn.py
+++ configs/video_id4_2gpu_trn.py
@@ -9,7 +9,7 @@ model = dict(
     backbone=dict(
         _delete_=True,
         type='TRN',
-        num_segment=300,
+        num_segment=50,
         input_dim=1024,
         output_dim=2048
     ),
@@ -25,5 +25,61 @@ optimizer = dict(
         'backbone': dict(lr_mult=0.01, decay_mult=1.0),
     }))
 
-# data = dict(samples_per_gpu=2, workers_per_gpu=1)
-data = dict(samples_per_gpu=8, workers_per_gpu=8)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375])
+
+train_pipeline = [
+    dict(type='LoadAnnotations',
+         replace_dict=dict(video=(
+             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/train_5k'))),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
+    dict(type='Pad', video_pad_size=(50, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'meta_info', 'gt_labels'])
+]
+
+val_pipeline = [
+    dict(type='LoadAnnotations',
+         replace_dict=dict(video=(
+             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/train_5k'))),
+    dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
+    dict(type='Pad', video_pad_size=(50, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'meta_info'])
+]
+
+test_pipeline = [
+    dict(type='LoadAnnotations',
+         replace_dict=dict(video=(
+             'tagging/tagging_dataset_train_5k/video_npy/Youtube8M/tagging',
+             'extracted_video_feats/L16_LN/test_5k_2nd'))),
+    dict(type='BertTokenize', bert_path='pretrained/bert', max_length=256),
+    dict(type='Pad', video_pad_size=(50, 1024), audio_pad_size=(300, 128)),
+    dict(type='Resize', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['video', 'meta_info'])
+]
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(type='TaggingDataset',
+               ann_file='dataset/tagging/GroundTruth/datafile/train.txt',
+               label_id_file='dataset/tagging/label_super_id.txt',
+               pipeline=train_pipeline),
+    val=dict(type='TaggingDataset',
+             ann_file='dataset/tagging/GroundTruth/datafile/val.txt',
+             label_id_file='dataset/tagging/label_super_id.txt',
+             pipeline=val_pipeline),
+    test=dict(type='TaggingDataset',
+              ann_file='dataset/tagging/GroundTruth/datafile/test_2nd.txt',
+              label_id_file='dataset/tagging/label_super_id.txt',
+              pipeline=test_pipeline))

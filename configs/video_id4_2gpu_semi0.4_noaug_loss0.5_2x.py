9660f3f506e89fe813ccb72703f0aed47e393b91
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-02 12:08:31 +0800

    Update video_id4_2gpu_semi0.4_noaug_loss0.5_2x.py

--- configs/video_id4_2gpu_semi0.4_noaug_loss0.5_2x.py
+++ configs/video_id4_2gpu_semi0.4_noaug_loss0.5_2x.py
@@ -1,5 +1,16 @@
 _base_ = 'video_id4_2gpu.py'
 
+train_total_iters = 20000
+
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[train_total_iters // 3 , 2 * train_total_iters // 3]
+)
+runner = dict(type='IterBasedRunner', max_iters=train_total_iters)
+
 
 model = dict(
     type='SemiSingleBranchModel',

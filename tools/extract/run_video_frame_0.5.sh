97d227b6c8c0a9df3b31100411f2ecb773f1572d
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-07-03 15:10:36 +0800

    upd

--- tools/extract/run_video_frame_0.5.sh
+++ tools/extract/run_video_frame_0.5.sh
@@ -1,4 +1,4 @@
-python tools/extract/extract_video_frame_0.5.py --video_path=dataset/videos/video_5k/train_5k --save_path=dataset/extracted_video_frame/train_5k
+python tools/extract/extract_video_frame_0.5.py --video_path=dataset/videos/video_5k/train_5k --save_path=dataset/extracted_video_frame/train_5k_0.5
 #python tools/extract/extract_video_frame.py --video_path=dataset/videos/video_5k/test_5k --save_path=dataset/extracted_video_frame/test_5k
 #python tools/extract/extract_video_frame.py --video_path=dataset/videos/test_5k_2nd --save_path=dataset/extracted_video_frame/test_5k_2nd
 # Max_len=115, Min_len=5, Median_len=60

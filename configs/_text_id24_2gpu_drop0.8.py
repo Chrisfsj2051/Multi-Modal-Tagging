0823f699b10f5bf7ee5e4d13d924eaa2d649a2b7
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-05 12:04:03 +0800

    upd

---
+++ configs/_text_id24_2gpu_drop0.8.py
@@ -0,0 +1,40 @@
+_base_ = [
+    '_base_/default_runtime.py', '_base_/schedules/schedule_1x_adam.py',
+    '_base_/models/text.py', '_base_/datasets/text.py'
+]
+
+norm_cfg = dict(type='BN1d')
+
+model = dict(
+    type='SingleBranchModel',
+    key='text',
+    backbone=dict(
+        type='TwoStreamTextCNN',
+        vocab_size=21129,
+        ebd_dim=300,
+        channel_in=256,
+        channel_out=1024,
+        filter_size=(2, 3, 4)
+    ),
+    head=dict(
+        type='SingleSEHead',
+        in_dim=1024,
+        gating_reduction=8,
+        out_dim=1024,
+        dropout_p=0.8,
+        cls_head_config=dict(
+            type='ClsHead',
+            in_dim=1024,
+            out_dim=82,
+            loss=dict(type='MultiLabelBCEWithLogitsLoss', loss_weight=8))
+    ))
+
+
+optimizer = dict(
+    lr=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone': dict(lr_mult=0.01, decay_mult=1.0),
+        }
+    )
+)

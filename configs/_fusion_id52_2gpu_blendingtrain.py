917ab68b9c042b08fd1eb22e804324d9f739345f
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-06 16:52:27 +0800

    upd

---
+++ configs/_fusion_id52_2gpu_blendingtrain.py
@@ -0,0 +1,14 @@
+_base_ = '_fusion_id52_2gpu.py'
+
+data = dict(
+    train=dict(ann_file='dataset/tagging/GroundTruth/datafile/blending_train_dev.txt'),
+    val=dict(ann_file='dataset/tagging/GroundTruth/datafile/blending_val_dev.txt')
+)
+
+train_total_iters = int(10000 * 0.9)
+lr_config = dict(policy='step',
+                 warmup='linear',
+                 warmup_iters=500,
+                 warmup_ratio=0.001,
+                 step=[train_total_iters // 3, 2 * train_total_iters // 3])
+runner = dict(type='IterBasedRunner', max_iters=train_total_iters)

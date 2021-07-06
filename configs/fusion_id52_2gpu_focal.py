41a0a78c868f76a4ec1b979c1062738cae46f7d7
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-06 17:56:37 +0800

    upd

--- configs/fusion_id52_2gpu_focal.py
+++ configs/fusion_id52_2gpu_focal.py
@@ -1,11 +1,11 @@
 _base_ = '_fusion_id52_2gpu.py'
 
 model = dict(
-    fusion_config=dict( cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5))),
+    fusion_config=dict( cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5, loss_weight=22))),
     branch_config=dict(
-        video=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5)))),
-        image=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5)))),
-        text=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5)))),
-        audio=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5))))
+        video=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5, loss_weight=22)))),
+        image=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5, loss_weight=22)))),
+        text=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5, loss_weight=22)))),
+        audio=dict(head=dict(cls_head_config=dict(loss=dict(type='MultiLabelBCEWithLogitsFocalLoss', gamma=1.5, loss_weight=22))))
     )
 )

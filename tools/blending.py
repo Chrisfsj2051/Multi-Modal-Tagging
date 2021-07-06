2eee9506bfb38e16b2a938cc53529158b6ba70ed
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-07-06 16:24:51 +0800

    Update blending.py

--- tools/blending.py
+++ tools/blending.py
@@ -11,6 +11,7 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 from sklearn.ensemble import GradientBoostingClassifier
+from tqdm import tqdm
 
 from mmt import build_dataloader, build_dataset
 from mmt.models.builder import build_model
@@ -55,9 +56,10 @@ def main():
     val_ds = build_dataset(blending_cfg.data.val)
     train_loader = build_dataloader(train_ds, samples_per_gpu=1, workers_per_gpu=1, dist=False, shuffle=False)
     val_loader = build_dataloader(val_ds, samples_per_gpu=1, workers_per_gpu=1, dist=False, shuffle=False)
-    gbdt = GradientBoostingClassifier(learning_rate=0.02, subsample=0.5, max_depth=6, n_estimators=30)
+    gbdt = [GradientBoostingClassifier(learning_rate=0.02, subsample=0.5, max_depth=6, n_estimators=30)
+            for _ in range(82)]
     feat_list_1, feat_list_2 = [], []
-    for data in train_loader:
+    for data in tqdm(train_loader):
         with torch.no_grad():
             pred_1 = model_1.forward(return_loss=False, **data)[0]['fusion']
             pred_2 = model_2.forward(return_loss=False, **data)[0]['fusion']
@@ -67,10 +69,41 @@ def main():
     feat_1 = np.concatenate(feat_list_1, 0)
     feat_2 = np.concatenate(feat_list_2, 0)
     feat = np.concatenate([feat_1, feat_2], 1)
+    gt_labels = train_ds.gt_label
 
-    feat_1, feat_2 = [], []
-    # for data in train_ds:
-    #     print('')
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # gt_labels[0] = list(range(82))
+    # gt_labels[1] = [81]
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    for cls in tqdm(range(82)):
+        binary_labels = [cls in lab for lab in gt_labels]
+        binary_labels = np.array(binary_labels).astype(np.int32)
+        gbdt[cls].fit(feat, binary_labels)
+
+    preds = []
+    feat_list_1, feat_list_2 = [], []
+    for data in tqdm(val_loader):
+        with torch.no_grad():
+            pred_1 = model_1.forward(return_loss=False, **data)[0]['fusion']
+            pred_2 = model_2.forward(return_loss=False, **data)[0]['fusion']
+        feat_1 = pred_1.cpu().numpy()
+        feat_2 = pred_2.cpu().numpy()
+        feat = np.concatenate([feat_1, feat_2], 1)
+        prob = [gbdt[i].predict_proba(feat)[0][1] for i in range(len(gbdt))]
+        preds.append(prob)
+    preds_fmt = [{'blending': [torch.FloatTensor(x)]} for x in preds]
+    eval_res = val_ds.evaluate(preds_fmt)
+    print(eval_res)
+
 
 
 
